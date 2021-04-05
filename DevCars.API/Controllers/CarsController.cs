5a5b339b819791b44748b4ae849bdb0f89daff4a
Luis Felipe <luisfelipekai@gmail.com>
2021-04-05 20:51:04 -0300

    Pós-Live Encerramento.

--- DevCars.API/Controllers/CarsController.cs
+++ DevCars.API/Controllers/CarsController.cs
@@ -1,8 +1,13 @@
-﻿using DevCars.API.Entities;
+﻿using Dapper;
+using DevCars.API.Entities;
 using DevCars.API.InputModels;
 using DevCars.API.Persistence;
 using DevCars.API.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System.Linq;
 
 namespace DevCars.API.Controllers
@@ -11,9 +16,14 @@ namespace DevCars.API.Controllers
     public class CarsController : ControllerBase
     {
         private readonly DevCarsDbContext _dbContext;
-        public CarsController(DevCarsDbContext dbContext)
+        private readonly string _connectionString;
+        public CarsController(DevCarsDbContext dbContext, IConfiguration configuration)
         {
             _dbContext = dbContext;
+
+            // SE EU UTILIZAR O DBCONTEXT, E UTILIZAR O INMEMORY VAI DAR ERRO
+            // _connectionString = _dbContext.Database.GetDbConnection().ConnectionString; 
+            _connectionString = configuration.GetConnectionString("DevCarsCs");
         }
 
         // GET api/cars
@@ -29,6 +39,15 @@ namespace DevCars.API.Controllers
                 .ToList();
 
             return Ok(carsViewModel);
+
+            //using (var sqlConnection = new SqlConnection(_connectionString))
+            //{
+            //    var query = "SELECT Id, Brand, Model, Price FROM Cars WHERE Status = 0";
+
+            //    var carsViewModel = sqlConnection.Query<CarItemViewModel>(query);
+
+            //    return Ok(carsViewModel);
+            //}
         }
 
         // GET api/cars/1
@@ -61,7 +80,28 @@ namespace DevCars.API.Controllers
         }
 
         // POST api/cars
+        /// <summary>
+        /// Cadastrar um Carro
+        /// </summary>
+        /// <remarks>
+        /// Requisição de exemplo:
+        /// {
+        ///     "brand": "Honda",
+        ///     "model": "Civic",
+        ///     "vinCode": "abc123",
+        ///     "year": 2021,
+        ///     "price": 100000,
+        ///     "color": "Cinza",
+        ///     "productionDate": "2021-04-05"
+        /// }
+        /// </remarks>
+        /// <param name="model">Dados de um novo Carro</param>
+        /// <returns>Objeto recém-criado</returns>
+        /// <response code="201">Objeto criado com sucesso.</response>
+        /// <response code="400">Dados inválidos.</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Post([FromBody] AddCarInputModel model)
         {
             // SE O CADASTRO FUNCIONAR, RETORNA CREATED (201)
@@ -87,7 +127,26 @@ namespace DevCars.API.Controllers
         }
 
         // PUT api/cars/1
+        /// <summary>
+        /// Atualizar dados de um Carro
+        /// </summary>
+        /// <remarks>
+        /// Requisição de Exemplo:
+        /// {
+        ///     "color": "Vermelho",
+        ///     "price": 100000
+        /// }
+        /// </remarks>
+        /// <param name="id">Identificador de um Carro</param>
+        /// <param name="model">Dados de alteração</param>
+        /// <returns>Não tem retorno.</returns>
+        /// <response code="204">Atualização bem-sucedida</response>
+        /// <response code="400">Dados inválidos.</response>
+        /// <response code="404">Carro não encontrado.</response>
         [HttpPut("{id}")]
+        //[ProducesResponseType(StatusCodes.Status204NoContent)]
+        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+        //[ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Put(int id, [FromBody] UpdateCarInputModel model)
         {
             // SE A ATUALIZAÇÃO FUNCIONAR, RETORNA 204 NO CONTENT
@@ -105,6 +164,13 @@ namespace DevCars.API.Controllers
             car.Update(model.Color, model.Price);
 
             _dbContext.SaveChanges();
+
+            //using (var sqlConnection = new SqlConnection(_connectionString))
+            //{
+            //    var query = "UPDATE Cars SET Color = @color, Price = @price WHERE Id = @id";
+
+            //    sqlConnection.Execute(query, new { color = car.Color, price = car.Price, car.Id });
+            //}
 
             return NoContent();
         }
--- DevCars.API/Startup.cs
+++ DevCars.API/Startup.cs
@@ -11,7 +11,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace DevCars.API
@@ -30,25 +32,38 @@ namespace DevCars.API
         {
             var connectionString = Configuration.GetConnectionString("DevCarsCs");
 
-            services.AddDbContext<DevCarsDbContext>(options => options.UseSqlServer(connectionString));
-            // services.AddDbContext<DevCarsDbContext>(options => options.UseInMemoryDatabase("DevCars"));
+            // services.AddDbContext<DevCarsDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddDbContext<DevCarsDbContext>(options => options.UseInMemoryDatabase("DevCars"));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DevCars.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { 
+                    Title = "DevCars.API", 
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Luis Felipe",
+                        Email = "luisfelipe@umemail.com.br",
+                        Url = new Uri("https://luisdev.com.br")
+                    }                
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DevCars.API v1"));
-            }
+            //if (env.IsDevelopment())
+            //{
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DevCars.API v1"));
+            //}
 
             app.UseHttpsRedirection();
 
